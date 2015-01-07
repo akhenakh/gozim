456a6b1cf1b6921779680d0b587b09928806208c
mattn <mattn.jp@gmail.com>
2015-01-07 12:40:29 +0900

    Use log.Fatal instead of panic like gozimhttpd

--- cmd/gozimindex/main.go
+++ cmd/gozimindex/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -41,7 +42,7 @@ func main() {
 	flag.Parse()
 
 	if *path == "" {
-		panic("provide a zim file path")
+		log.Fatal(errors.New("provide a zim file path"))
 	}
 
 	z, err := zim.NewReader(*path, false)
